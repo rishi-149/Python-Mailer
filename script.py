5ca5e6e0e0703d4b3f3f59c2358757f2613c926a
unknown <rishigupta1492000@gmail.com>
2025-12-29 16:07:09 +0530

    Added codes

---
+++ script.py
@@ -0,0 +1,35 @@
+import smtplib
+from email.mime.txt import MIMEtext  # MIMEText is a class that represents the text of the email
+from email.mime.multipart import MIMEMultipart  # MIMEMultipart is a class that represents the email message itself
+import os
+
+def send_mail(workflow_name, repo_name):
+    #email details
+    sender_email = 0s.getenv('SENDER_PASSWORD')
+    sender_password = os.getenv('SENDER_PASSWORD')
+    receiver_email = os.getenv('RECEIVER_EMAIL')
+
+    # Email message
+    subject = f"workflow {workflow_name} failed for repo {repo_name}"
+    body = f"Hi, the workflow {workflow_name} failed for the repo {repo_name}. Please check the logs for more details.\nMore details: \nRun_ID: {workflow_run_id}"
+
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = subject
+    msg.attach(MIMEtext(body, 'Plain'))
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com',587)
+        server.starttls()
+        server.login(sender_email, sender_password)
+        text = msg.as_string
+        server.sendmail(sender_email, receiver_email, text)
+        server.quit()
+
+        print('Email send successfully')
+    except Exception as e:
+        print(f'Error: {e}')
+
+
+send_mail(os.getenv('WORKFLOW_NAME'), os.getenv('REPO_NAME'), os.getenv('WORKFLOW_RUN_ID'))
